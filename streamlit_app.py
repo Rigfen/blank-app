0b77551e24b8bb41bbf0cbb341d7995d4ec64b8f
Rigfen <Rigfen@gmail.com>
2025-12-04 13:29:35 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,8 +61,8 @@ with left_col:
 
 
 with right_col:
-    product_image = Image.open(Assets_DIR / "download.png")
+    product_image = Image.open(Assets_DIR / "Organize_Away.png")
     st.image(product_image, width=450 )
 with left_col:
-    product_image = Image.open(Assets_DIR / "Organize_Away.png")
+    product_image = Image.open(Assets_DIR / "download.png")
     st.image(product_image, width=600 )
